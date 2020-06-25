29574383fe1954a2e55b13da613071eb98ed25c0
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-06-25 15:39:47 +0900

    클릭기능 추가

--- Assets/02 Scripts/Vivecontroller.cs
+++ Assets/02 Scripts/Vivecontroller.cs
@@ -6,23 +6,47 @@ public class Vivecontroller : MonoBehaviour
 {
 
     //컨트롤러 정의
- public SteamVR_Input_Sources leftHand;
- public SteamVR_Input_Sources rightHand;
- public SteamVR_Input_Sources anyHand;
+    public SteamVR_Input_Sources leftHand;
+    public SteamVR_Input_Sources rightHand;
+    public SteamVR_Input_Sources any;
 
-//컨트롤러 입력값 정의
-public SteamVR_Action_Boolean trigger;
+    //컨트롤러 입력값 정의
+    public SteamVR_Action_Boolean trigger;
+    
+    //트랙패드 클릭
+    public SteamVR_Action_Boolean trackPadCilck = SteamVR_Actions.default_Teleport;
+    public SteamVR_Action_Boolean trackPadTouch = SteamVR_Actions.default_TrackPadTouch;
+    public SteamVR_Action_Vector2 trackPadPosition = SteamVR_Actions.default_TrackPadPosition;
 
- void Awake()
-{
-    trigger = SteamVR_Actions.default_InteractUI; 
-}
+    void Awake()
+    {
+        trigger = SteamVR_Actions.default_InteractUI; 
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if(SteamVR_Actions.default_InteractUI.GetStateDown(leftHand))
+        // 왼손 컨트롤러 트리거를 눌렀을 때 
+        if(trigger.GetStateDown(leftHand))
         {
             Debug.Log("click left trigger");
         }
+
+        //오른손 컨트롤러 트리거를 눌렀다가 떼었을때 
+        if(trigger.GetStateUp(rightHand)){
+            Debug.Log ("Released Triiger button");
+        }
+    
+        if(trackPadCilck.GetStateDown(any)){
+            Debug.Log ("TrackPad Click");
+        }
+
+        if(trackPadTouch.GetState(any))
+        {
+        Vector2 pos = trackPadPosition.GetAxis(any);  
+        Debug.Log($"Touch pos x= {pos.x}/y={pos.y}");
+        }     
     }
+
 }
