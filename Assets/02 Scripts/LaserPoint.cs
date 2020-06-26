0b98c6adb02866fe04d4062114368b4ce83b0652
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-06-26 10:36:01 +0900

    렉팅글 기능 추가

--- Assets/02 Scripts/LaserPoint.cs
+++ Assets/02 Scripts/LaserPoint.cs
@@ -11,7 +11,7 @@ public class LaserPoint : MonoBehaviour
     
     [SerializeField] //프라이빗이지만 인스펙터창에 보여짐
     private GameObject pointerPrefab;
-    private GameObject Pointer;
+    private GameObject pointer;
 
     //라인렌더러 속성변수
     private LineRenderer line;
@@ -21,6 +21,9 @@ public class LaserPoint : MonoBehaviour
     public Color defaltColor = Color.green;
     public Color clikedColor = Color.blue;
 
+    //Raycast
+    private RaycastHit hit;
+    private Transform tr;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +35,10 @@ public class LaserPoint : MonoBehaviour
 
         pointerPrefab = Resources.Load<GameObject>("Pointer");
         //pointerPrefab = Resources.Load("Pointer") as GameObject;
-        Pointer = Instantiate<GameObject>(pointerPrefab,this.transform);//(pointerPrefab,위치,각도, 부모)
+   
+        pointer = Instantiate<GameObject>(pointerPrefab,this.transform);//(pointerPrefab,위치,각도, 부모)
+        tr = GetComponent<Transform>();
+
     }
 
  
@@ -49,5 +55,23 @@ public class LaserPoint : MonoBehaviour
         line.endWidth = 0.005f;
 
     
+    }
+
+    void Update()
+    {
+        if(Physics.Raycast(tr.position,tr.forward, out hit, distance))//시작좌표. 광선의 방향(0.0.1), 맞은 오브젝트의 정보, 광선의 거리
+        {
+            line.SetPosition(1,new Vector3(0,0,hit.distance));//라인 렌더러의 끝점을 오브젝트의 거리 까지만 한다. 
+        
+            pointer.transform.position = hit.point + (hit.normal*0.01f);
+            pointer.transform.rotation = Quaternion.LookRotation(hit.normal);// 물체와 부딧
+        }
+
+        else {
+            pointer.transform.position = tr.position +(tr.forward * distance); //하늘을 바라봤을 때 수직으로보게한다 
+            pointer.transform.rotation = Quaternion.LookRotation(tr.forward); //올일러를 하면 오류가있을수있음  벡터의 각도를 쿼터니온 각도로 변환해서 넣어준다. 
+        }
+
+
     }
 }
